0e835c054ad4b6481ee88f1c37f302c4efe26bdc
Jimbahh <Jimbahh@DESKTOP-O8H641I>
2018-10-24 08:35:11 +1100

    Minor GUI adjustment

--- src/diceGame/Gui.java
+++ src/diceGame/Gui.java
@@ -10,30 +10,32 @@ public static void main(String args[]) {
 //Creating the Frame
 	JFrame frame = new JFrame("Dice Game - New Game");
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setSize(400, 400);
+	frame.setSize(350, 400);
 
 	JPanel panel = new JPanel(); 
-	JPanel panel1 = new JPanel(); 
-	JLabel mainLabel = new JLabel("NEW GAME");
+	JPanel buttonPanel = new JPanel(); 
+	JLabel mainLabel = new JLabel("New Game", SwingConstants.CENTER);
+	mainLabel.setFont(new Font("", Font.BOLD, 20));
 	JLabel p1Label = new JLabel("Player 1:");
 	JLabel p2Label = new JLabel("Player 2:");
-	JTextField p1Name = new JTextField(12);
-	JTextField p2Name = new JTextField(12);
+	JTextField p1Name = new JTextField("Player 1", 20);
+	JTextField p2Name = new JTextField("Player 2", 20);
 	JButton startButton = new JButton("Start");
 	JButton exitButton = new JButton("Exit");
 	panel.add(p1Label);
 	panel.add(p1Name);
 	panel.add(p2Label);
 	panel.add(p2Name);
-	panel1.add(startButton);
-	panel1.add(exitButton);
+	buttonPanel.add(startButton);
+	buttonPanel.add(exitButton);
 
 	
 	//Adding Components to the frame.
 
 	frame.getContentPane().add(BorderLayout.NORTH, mainLabel);
 	frame.getContentPane().add(BorderLayout.CENTER, panel);
-	frame.getContentPane().add(BorderLayout.SOUTH, mainLabel);
+
+	frame.getContentPane().add(BorderLayout.SOUTH, buttonPanel);
 
 	
 	frame.setVisible(true);
